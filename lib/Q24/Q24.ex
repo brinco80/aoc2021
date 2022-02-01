492ddba47c3a726c802428bb9cc0b3989fce8be8
Diego Sepúlveda <brinco@gmail.com>
2022-02-01 08:48:23 -0300

    Added Q25 and Q24

--- lib/Q24/Q24.ex
+++ lib/Q24/Q24.ex
@@ -25,9 +25,8 @@ defmodule Q24 do
     )
   end
 
-  def execute(program, number) do
+  def execute(program, digits) do
     state0 = %{"x" => 0, "y" =>  0, "z" => 0, "w" => 0}
-    digits = Integer.digits(number)
     program
     |> Enum.reduce({state0, digits},
       fn op, {state, n} -> do_operation(op, n, state) end
@@ -48,23 +47,163 @@ defmodule Q24 do
   def do_operation({"eql", [x,y]}, n, state), do: {%{state | x =>  if(state[x] ==y, do: 1, else: 0)}, n}
 
 
+  def simple_program(p) do
+    p
+    |> Enum.chunk_every(18)
+    |> Enum.map(
+      fn p ->
+        [
+          _inp,
+          _mulx0,
+          _addxz,
+          _modx26,
+          {"div", ["z", zd]},
+          {"add", ["x", c1]},
+          _eqlxw,
+          _eqlx0,
+          _muly0,
+          _addy25,
+          _mulyx,
+          _addy1,
+          _mulzy,
+          _muly02,
+          _addyw,
+          {"add", ["y", c2]},
+          _mul_yx,
+          _addzy
+        ] = p
+        fn d,z -> program_step(zd,c1,c2,d,z) end
+      end
+    )
+  end
 
-  def part_i(file \\ "lib/Q24/test") do
+  def memoization() do
+    fn fun ->
+      fn args, acc ->
+        acc =
+          if(!acc[args]) do
+            Map.put(acc, args, fun.(args))
+          else
+            acc
+          end
+        {acc, Map.get(acc, args)}
+      end
+    end
+
+  end
+
+  def program_step(zd, c1, c2, d, z) do
+    x = rem(z,26)
+    z = div(z,zd)
+    if(x + c1 != d) do
+      26*z + d+c2
+    else
+      z
+    end
+  end
+
+  def fast_execute(simple_program, digits) do
+    state0 = 0
+    simple_program
+    |> Enum.reduce({state0, digits},
+      fn step, {state, n} ->
+        [d | rest] = n
+        state = step.(d, state)
+        {state, rest}
+      end
+    )
+  end
+
+  def first_digits([d13, d12, d11, d10, d9 | rest]) do
+    {26*26*26*26*(d13+5) + 26*26*26*(d12+5) + 26*26*(d11+1) + 26*(d10+15) + d9+2, rest}
+  end
+
+  def faster_execute(simple_program, digits) do
+    {z, rest} =first_digits(digits)
+
+    simple_program
+    |> Enum.reduce({z, rest},
+    fn step, {state, n}->
+      [d | rest] = n
+      state = step.(d, state)
+      {state, rest}
+    end
+  )
+
+  end
+
+  def part_ia(file \\ "lib/Q24/data") do
     program = read_and_parse(file)
     n = 11111111111111*9
 
     n..11111111111111
+    |> Stream.map(fn n -> Integer.digits(n) end)
     |> Stream.filter( # filter numbers with 0
-      fn n ->
-        Integer.digits(n) |> Enum.all?( fn d -> d>0 end )
+      fn ns->
+        ns |> Enum.all?( fn d -> d>0 end )
       end
     )
     |> Enum.reduce_while(nil,
-      fn n, _ ->
-        IO.puts("Test #{n}")
-        case execute(program, n) do
+      fn digits, _ ->
+        #IO.puts("Test #{n}")
+        case execute(program, digits) do
           %{"z" => z} when  z==0  -> {:halt, n}
           _ -> {:cont, nil}
+        end
+      end
+    )
+  end
+
+  def part_ib(file \\ "lib/Q24/data") do
+    program = read_and_parse(file)
+    n = 11111111111111*9
+
+    sp0 = simple_program(program)
+
+    n..11111111111111
+    |> Stream.map(fn n -> Integer.digits(n) end)
+    |> Stream.filter( # filter numbers with 0
+      fn ns ->
+        ns |> Enum.all?( fn d -> d>0 end )
+      end
+    )
+    |> Stream.take(100_000_000)
+    |> Enum.reduce_while(0,
+      fn digits, i ->
+        if(rem(i,10_000_000) == 0, do: IO.puts("Testing #{Integer.undigits(digits)}"))
+
+        case fast_execute(sp0, digits) do
+          {0, []} -> {:halt, n}
+          {_z, []} -> {:cont, i+1}
+        end
+      end
+    )
+  end
+
+  def part_ic(file \\ "lib/Q24/data") do
+    program = read_and_parse(file)
+    n = 11111111111111*9
+
+    sp0 = simple_program(program)
+
+    sp = sp0 |> Enum.take(-(length(sp0) - 5))
+
+    #n..11111111111111
+    91111111111111..n
+    |> Stream.map(fn n -> Integer.digits(n) end)
+    |> Stream.filter( # filter numbers with 0
+      fn ns ->
+        ns |> Enum.all?( fn d -> d>0 end )
+      end
+    )
+#    |> Stream.take(100_000_000)
+    |> Enum.reduce_while(0,
+      fn digits, i ->
+        if(rem(i,10_000_000) == 0, do: IO.puts("Testing #{Integer.undigits(digits)}"))
+
+        case faster_execute(sp, digits) do
+          {0, []} -> {:halt, n}
+          {_z, []} -> {:cont, i+1}
         end
       end
     )
---
+++ lib/Q25/Q25.ex
@@ -0,0 +1,151 @@
+defmodule Q25 do
+
+
+  def read_and_parse(filename) do
+    # read file
+    {:ok, data_str} = File.read(filename)
+
+    sea_floor = data_str
+    |> String.split("\n")
+    |> Enum.with_index(fn element, i -> {i, element} end)
+    |> Enum.flat_map(
+        fn {i, line} ->
+          line
+          |> String.split("", trim: true)
+          |> Enum.with_index(fn element, j -> {j, element} end)
+          |> Enum.map(
+            fn {j,z} ->
+              {{i,j}, z}
+            end
+          )
+        end
+      )
+    |> Map.new
+    |> Map.filter(fn {_k, v} -> v != "" end) # Remove empty spaces
+
+    boundaries = sea_floor
+    |> Map.keys()
+    |> Enum.reduce({-1, -1},
+      fn {x,y}, {max_x, max_y} ->
+        x = max(x, max_x)
+        y = max(y, max_y)
+        {x, y}
+      end
+    )
+
+    {sea_floor, boundaries}
+  end
+
+  def state_to_string(state) do
+    keys = state
+    |> Map.keys
+
+    {max_i, max_j} = for {i,j} <- keys, reduce: {-1,-1} do
+      {m,n} -> {max(m,i), max(n,j)}
+    end
+
+    for i <- 0..max_i, j<- 0..max_j+1 do
+      if(j == max_j+1, do: "\n", else: Map.get(state, {i,j}, " "))
+    end |> Enum.join()
+  end
+
+  def transition_to_string(from, to) do
+    keys = from
+    |> Map.keys
+
+    {max_i, max_j} = for {i,j} <- keys, reduce: {-1,-1} do
+      {m,n} -> {max(m,i), max(n,j)}
+    end
+
+    for i <- 0..max_i, j<- 0..2*max_j+3 do
+      case j do
+        x when x <= max_j -> Map.get(from, {i,x}, " ")
+        x when x == max_j+1 -> "    "
+        x when x <= 2*max_j + 2 -> Map.get(to, {i,x-(max_j+2)}, " ")
+        _ -> "\n"
+      end
+    end |> Enum.join()
+  end
+
+  def print_state(state) do
+    state |> state_to_string |> IO.write
+  end
+
+  def update_state(state0, {mx,my}) do
+    horizontal = 0..mx
+    |> Enum.reduce(state0,
+      fn x, state ->
+        0..my
+        |> Enum.reduce(
+          state,
+          fn y, state2 ->
+            p = {x,y}
+            case state[p] do
+              ">" ->
+                y1 = rem(y+1, my+1)
+
+                if(state[{x, y1}] == ".") do
+                    state2
+                    |> Map.put({x,y}, ".")
+                    |> Map.put({x,y1}, ">")
+                else
+                  state2
+                end
+              _ ->
+                state2
+            end
+          end
+        )
+      end
+    )
+
+    0..my
+    |> Enum.reduce(horizontal,
+      fn y, state ->
+        0..mx
+        |> Enum.reduce(
+          state,
+          fn x, state2 ->
+            p = {x,y}
+            case state[p] do
+              "v" ->
+                x1 = rem(x+1, mx+1)
+                if(state[{x1, y}] == ".") do
+                    state2
+                    |> Map.put({x,y}, ".")
+                    |> Map.put({x1,y}, "v")
+                else
+                  state2
+                end
+              _ ->
+                state2
+            end
+          end
+        )
+      end
+    )
+  end
+
+  def part_i(file \\ "lib/Q25/test") do
+    {state0, boundaries} = read_and_parse(file)
+
+    Stream.iterate(0, &(&1 + 1))
+    |> Enum.reduce_while(
+      state0,
+      fn n, state ->
+        new_state = update_state(state, boundaries)
+
+        if state == new_state do
+          IO.puts("Stopped at #{n+1}")
+          {:halt, new_state}
+        else
+          #IO.puts("Step #{n}")
+          #print_state(new_state)
+          {:cont, new_state}
+        end
+      end
+    )
+  end
+
+
+end
